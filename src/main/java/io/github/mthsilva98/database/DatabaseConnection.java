442ee6ff620af1ed9e069def950c0c9662f8e147
Matheus <mthsilva1998@gmail.com>
2024-12-10 17:08:17 -0300

    Feito o Database Connection

--- src/main/java/io/github/mthsilva98/database/DatabaseConnection.java
+++ src/main/java/io/github/mthsilva98/database/DatabaseConnection.java
@@ -1,4 +1,40 @@
 package io.github.mthsilva98.database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnection {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/sistema_biblioteca";
+    private static final String USUARIO = "root";
+    private static final String SENHA = "123456ma";
+
+    private static Connection conexao;
+
+    public static Connection getConexao() {
+        if (conexao == null) {
+            try {
+                conexao = DriverManager.getConnection(URL, USUARIO, SENHA);
+                System.out.println("Conexão com o banco de dados estabelecida com sucesso!");
+            } catch (SQLException e) {
+                System.err.println("Erro ao conectar ao banco de dados: " + e.getMessage());
+                // Aqui você pode adicionar tratamento de erro mais robusto, como lançar uma exceção
+                // ou registrar o erro em um arquivo de log.
+            }
+        }
+        return conexao;
+    }
+
+
+    public static void fecharConexao() {
+        if (conexao != null) {
+            try {
+                conexao.close();
+                System.out.println("Conexão com o banco de dados fechada.");
+            } catch (SQLException e) {
+                System.err.println("Erro ao fechar a conexão com o banco de dados: " + e.getMessage());
+            }
+        }
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module io.github.mthsilva98.sistemabiblioteca {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens io.github.mthsilva98 to javafx.fxml;
